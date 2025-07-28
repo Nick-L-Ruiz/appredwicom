e65ca6575bbf74778c220fcc30a0f334885586c2
Nick L. Ruiz <nickl.ruiz.contacto@gmail.com>
2025-07-28 13:28:07 -0600

    app en flutter para redwicom, vistas de la version de tecnico

---
+++ redwicom/lib/screens/main_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:redwicom/screens/login_screen.dart';
+
+class ScreenRedwicom extends StatelessWidget {
+  const ScreenRedwicom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginScreen();
+  }
+}
